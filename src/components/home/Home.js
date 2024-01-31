3d9abf65a1528f5d7d58d6767a5bc3f4f95ecc6c
kubajarmuzek <102328573+kubajarmuzek@users.noreply.github.com>
2024-01-31 22:05:35 +0100

    delete button added to the table

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { signOut } from 'firebase/auth';
+import { signOut } from "firebase/auth";
 import { auth } from '../../firebase';
 import { useNavigate } from 'react-router-dom';
-import { ref, get } from 'firebase/database';
+import { ref, get, remove } from 'firebase/database';
 import { getCurrentUserUID, database } from '../../firebase';
-import { useTable, useSortBy, useFilters } from 'react-table';
 
 import Sidebar from '../Sidebar';
 import './home.css';
@@ -16,54 +15,78 @@ const Home = () => {
   const [error, setError] = useState(null);
   const userUID = getCurrentUserUID();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (userUID) {
-          const databaseRef = ref(database, `users/${userUID}`);
+  const fetchData = async () => {
+    try {
+      if (userUID) {
+        const databaseRef = ref(database, `users/${userUID}`);
 
-          const snapshot = await get(databaseRef);
+        const snapshot = await get(databaseRef);
 
-          if (snapshot.exists()) {
-            const dataObject = snapshot.val();
-            const dataAsArray = Object.values(dataObject);
+        if (snapshot.exists()) {
+          const dataObject = snapshot.val();
+          const dataAsArray = Object.values(dataObject);
 
-            // Organize data into separate arrays based on metric names
-            const metricArrays = {};
+          const metricArrays = {};
 
-            dataAsArray.forEach((item) => {
-              const metricName = item.label;
+          dataAsArray.forEach((item) => {
+            const metricName = item.label;
 
-              if (!metricArrays[metricName]) {
-                metricArrays[metricName] = [];
-              }
+            if (!metricArrays[metricName]) {
+              metricArrays[metricName] = [];
+            }
 
-              metricArrays[metricName].push({
-                ...item,
-                day: item.date,
-              });
+            metricArrays[metricName].push({
+              ...item,
+              day: item.date,
             });
-            // Set the organized data and param names to state
-            setData(metricArrays);
-            setParamNames(Object.keys(metricArrays));
-          } else {
-            setError("No data found in the database for the current user.");
-          }
-        } else {
-          setError("User not authenticated");
-        }
-      } catch (error) {
-        setError(error.message);
-      }
-    };
+          });
 
+          setData(metricArrays);
+          setParamNames(Object.keys(metricArrays));
+        } else {
+          setError("No data found in the database for the current user.");
+        }
+      } else {
+        setError("User not authenticated");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleDelete = async (paramName, day) => {
+    try {
+      const formattedDay = day.replace(/\//g, '_');
+  
+      const userRef = ref(database, `users/${getCurrentUserUID()}/${formattedDay}${paramName}`);
+      
+      const snapshot = await get(userRef);
+
+      if (snapshot.exists()) {
+        await remove(userRef);
+        console.log(`Successfully deleted ${paramName} data for day ${formattedDay}`);
+
+        await fetchData();
+      } else {
+        console.log(`${paramName} data for day ${formattedDay} does not exist`);
+      }
+    } catch (error) {
+      console.error("Error deleting data:", error.message);
+    }
+  };
+  
+  
+
+  useEffect(() => {
     fetchData();
+    console.log(data);
+
   }, [userUID]);
 
+  
   if (!getCurrentUserUID()) {
     navigate("/auth");
   }
-
   const [showSidebar, setShowSidebar] = useState(false);
   const hoverStyles = {
     marginLeft: '300px',
@@ -71,101 +94,82 @@ const Home = () => {
   };
   const defaultStyle = {
     marginLeft: 0,
-  };
+  }
   const handleClick = () => {
-    setShowSidebar((showSidebar) => !showSidebar);
-  };
+    setShowSidebar(showSidebar => !showSidebar);
+  }
   const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        navigate("/auth");
-        console.log("Signed out successfully");
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
-      });
-  };
-
-  // Create columns for react-table based on paramNames
-  const columns = React.useMemo(
-    () =>
-      paramNames.map((paramName) => ({
-        Header: paramName,
-        columns: [
-          { Header: 'Label', accessor: 'label' },
-          { Header: 'Day', accessor: 'day' },
-          { Header: 'Value', accessor: paramName },
-          // Add additional columns as needed
-        ],
-      })),
-    [paramNames]
-  );
-
-  // Initialize the react-table instance
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({ columns, data }, useFilters, useSortBy);
+    signOut(auth).then(() => {
+      navigate("/auth");
+      console.log("Signed out successfully")
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage)
+    });
+  }
 
   return (
     <div className="home--body" style={showSidebar ? hoverStyles : defaultStyle}>
-      <div className="home--top">
-        <span className="menu--bars" onClick={handleClick}>
-          <i className="fa-solid fa-bars fa"></i>
-        </span>
-        <div className="home--logout">
-          <button onClick={handleLogout}>Logout</button>
+      <div className='home--top'>
+        <span className="menu--bars" onClick={handleClick}><i className="fa-solid fa-bars fa"></i></span>
+        <div className='home--logout'>
+          <button onClick={handleLogout}>
+            Logout
+          </button>
         </div>
       </div>
       {showSidebar && <Sidebar handleClick={handleClick} className="menu--sideNav" />}
 
       <div>
-        <p>Welcome Home</p>
+        <p>
+          Welcome Home
+        </p>
       </div>
 
-      <div>
+      <div className='home--table'>
         {error && <div>Error: {error}</div>}
         {paramNames.length > 0 && (
-          <table {...getTableProps()} style={{ width: '100%' }}>
+          <table>
             <thead>
-              {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                      {column.render('Header')}
-                      {/* Add a sort indicator */}
-                      <span>
-                        {column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''}
-                      </span>
-                      {/* Add a filter input */}
-                      <div>{column.canFilter ? column.render('Filter') : null}</div>
-                    </th>
-                  ))}
-                </tr>
-              ))}
+              <tr>
+                <th>Metric</th>
+                <th>Day</th>
+                <th>Value</th>
+                <th>Delete</th>
+              </tr>
             </thead>
-            <tbody {...getTableBodyProps()}>
-              {rows.map((row) => {
-                prepareRow(row);
-                return (
-                  <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => (
-                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                    ))}
+            <tbody>
+              {paramNames.sort().map((paramName) => (
+                <React.Fragment key={paramName}>
+                  <tr>
+                    <th>{paramName}</th>
+                    <th>Day</th>
+                    <th>Value</th>
+                    <th>Delete</th>
                   </tr>
-                );
-              })}
+                  {data[paramName].map((item) => (
+                    <tr key={item.day}>
+                      <td>{item.label}</td>
+                      <td>{item.day}</td>
+                      <td>{item[paramName]}</td>
+                      <td>
+                        <button
+                          onClick={() => handleDelete(paramName, item.day)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </React.Fragment>
+              ))}
             </tbody>
           </table>
         )}
       </div>
     </div>
   );
-};
+}
 
 export default Home;
