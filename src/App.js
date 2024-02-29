f949801fb9b74af43b4b1d5960b120cff2e12d17
kubajarmuzek <jakub.jarmuzek@o2.pl>
2024-02-29 09:49:10 +0100

    fix routing error

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@ import Questionary from './components/questionary/Questionary';
 import Corelation from './components/corelation/Corelation';
 import Auth from './components/auth/Auth';
 import Charts from './components/charts';
-import { BrowserRouter as Router} from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 
-
 function App() {
- 
   return (
-    <Router basename='/performance-monitoring-v2'>
+    <Router>
       <div>
         <section>                              
-            <Routes>                                                                        
-                <Route path="/" element={<Home/>}/>
-                <Route path="auth" element={<Auth/>}/>
-                <Route path="charts" element={<Charts/>}/>
-                <Route path="questionary" element={<Questionary/>}></Route>
-                <Route path="corelation" element={<Corelation/>}></Route>
-            </Routes>                    
+          <Routes>                                                                        
+            <Route path="/" element={<Home/>}/>
+            <Route path="auth" element={<Auth/>}/>
+            <Route path="charts" element={<Charts/>}/>
+            <Route path="questionary" element={<Questionary/>}></Route>
+            <Route path="corelation" element={<Corelation/>}></Route>
+          </Routes>                    
         </section>
       </div>
     </Router>
